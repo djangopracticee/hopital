255a5fffeacc7ae451a3e9388b5291e8332cab64
phevab <phevab1@gmail.com>
2025-08-26 22:13:12 +0000

    added doc templates

--- doctors/views.py
+++ doctors/views.py
@@ -4,13 +4,4 @@ from .models import Doctor
 # Create your views here.
 
 def dashboard(request):
-    all_doctors = Doctor.objects.all()
-    total_doctors = len(all_doctors)
-
-    context = {
-        'count': total_doctors,
-    }
-
-
-
-    return render(request, 'doctors/dashboard.html', context)
+    return render(request, 'doctors/dashboard.html')
